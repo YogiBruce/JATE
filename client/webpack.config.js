515b9db52473916e3506475f6274acf7a0a39613
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2023-01-01 12:36:43 -0800

    module loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,8 +53,24 @@ module.exports = () => {
     ],
 
     module: {
+      //CSS Loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //Babel-loader for ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
