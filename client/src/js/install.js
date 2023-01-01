696519ae30b390355a64f04ac2bfaaa2b2d8248b
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2023-01-01 13:10:02 -0800

    database.js methods

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,7 +13,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
@@ -28,7 +27,6 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true)
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null
 });
